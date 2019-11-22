07ffd651022eb74225fe845a9a8f82dcfe73cf67
WEBZLG <webzlg@163.com>
2019-11-22 16:50:07 +0800

    00

--- unpackage/resources/H54770F00/www/js/script.js
+++ unpackage/resources/H54770F00/www/js/script.js
@@ -1,8 +1,13 @@
-// var baseurl = 'http://dev.ngdemo.cn/mufa/'; //服务器url
-// var imgurl = 'http://dev.ngdemo.cn/mufa/statics'; //图片url
+// var baseurl = 'http://mufa.ngdemo.cn/'; //服务器url
+// var imgurl = 'http://mufa.ngdemo.cn/statics'; //图片url
+// http://mufa.ngdemo.cn/
 var baseurl = 'http://192.168.0.22:8900/'; //服务器url
 var imgurl = 'http://192.168.0.22:8900/statics'; //图片url
-
+// 复制
+mui("body").on('tap', '#copy', function() {
+	var num = this.getAttribute("num");
+	copy_fun(num)
+});
 //  复制方法
 function copy_fun(copy) { //参数copy是要复制的文本内容
 	mui.plusReady(function() {
@@ -62,7 +67,7 @@ var util = {
 	}
 };
 // 修改信息
-function changeinfo(token, headimgurl, nickname, phone, code,fun) {
+function changeinfo(token, headimgurl, nickname, phone, code, fun) {
 	mui.ajax({
 		type: 'post',
 		url: baseurl + "mffl/updateSysUser",
@@ -77,10 +82,10 @@ function changeinfo(token, headimgurl, nickname, phone, code,fun) {
 			'token': token
 		},
 		data: {
-			'headImgUrl':headimgurl,
-			'nickname':nickname,
-			'phone':phone,
-			'code':code
+			'headImgUrl': headimgurl,
+			'nickname': nickname,
+			'phone': phone,
+			'code': code
 		},
 		success: function(data) {
 			console.log(data);
@@ -98,4 +103,61 @@ function changeinfo(token, headimgurl, nickname, phone, code,fun) {
 			console.log(err)
 		}
 	});
-}
+};
+//扩展mui.showLoading
+(function($, window) {
+    //显示加载框
+    $.showLoading = function(message,type) {
+        if ($.os.plus && type !== 'div') {
+            $.plusReady(function() {
+                plus.nativeUI.showWaiting(message);
+            });
+        } else {
+            var html = '';
+            html += '<i class="mui-spinner mui-spinner-white"></i>';
+            html += '<p class="text">' + (message || "数据加载中") + '</p>';
+
+            //遮罩层
+            var mask=document.getElementsByClassName("mui-show-loading-mask");
+            if(mask.length==0){
+                mask = document.createElement('div');
+                mask.classList.add("mui-show-loading-mask");
+                document.body.appendChild(mask);
+                mask.addEventListener("touchmove", function(e){e.stopPropagation();e.preventDefault();});
+            }else{
+                mask[0].classList.remove("mui-show-loading-mask-hidden");
+            }
+            //加载框
+            var toast=document.getElementsByClassName("mui-show-loading");
+            if(toast.length==0){
+                toast = document.createElement('div');
+                toast.classList.add("mui-show-loading");
+                toast.classList.add('loading-visible');
+                document.body.appendChild(toast);
+                toast.innerHTML = html;
+                toast.addEventListener("touchmove", function(e){e.stopPropagation();e.preventDefault();});
+            }else{
+                toast[0].innerHTML = html;
+                toast[0].classList.add("loading-visible");
+            }
+        }   
+    };
+
+    //隐藏加载框
+      $.hideLoading = function(callback) {
+        if ($.os.plus) {
+            $.plusReady(function() {
+                plus.nativeUI.closeWaiting();
+            });
+        } 
+        var mask=document.getElementsByClassName("mui-show-loading-mask");
+        var toast=document.getElementsByClassName("mui-show-loading");
+        if(mask.length>0){
+            mask[0].classList.add("mui-show-loading-mask-hidden");
+        }
+        if(toast.length>0){
+            toast[0].classList.remove("loading-visible");
+            callback && callback();
+        }
+      }
+})(mui, window);
